23229fffa61c8db954e81e505953c38b8f2fa399
Vansh Anand <130173778+VanshAnand1@users.noreply.github.com>
2025-08-17 14:19:33 -0400

    8/ feat: can send values to database from index.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,17 @@
 import { Homepage } from "./components/Homepage.js";
-
+import axios from "axios";
 function App() {
-  return <Homepage></Homepage>;
+  const apiCall = () => {
+    axios.get("http://localhost:8080").then((data) => {
+      console.log(data);
+    });
+  };
+  return (
+    <div>
+      <Homepage></Homepage>
+      <button onClick={apiCall}>make api call</button>
+    </div>
+  );
 }
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -1,8 +1,33 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
+const cors = require("cors");
+app.use(cors());
+const mysql = require("mysql2/promise");
 
-app.get("/", (req, res) => {
-  res.send("Hello");
+const db = mysql.createPool({
+  connectionLimit: 10,
+  host: process.env.host,
+  user: process.env.user,
+  password: process.env.db_password,
+  database: process.env.database,
+  waitForConnections: true,
+});
+
+app.get("/", async (req, res) => {
+  // const username = req.body.username;
+  // const password = req.body.password;
+  try {
+    const [result] = await db.execute(
+      "INSERT INTO users (username, password) VALUES (?, ?)",
+      // [username, password]
+      ["testing", "123"]
+    );
+    res.json({ insertedId: result.insertId });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Insert failed");
+  }
 });
 
 app.listen(8080, () => {
