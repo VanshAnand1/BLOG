4797a5a56d9e535bcc25b8bde77c7d89d08afcf3
Vansh Anand <130173778+VanshAnand1@users.noreply.github.com>
2025-10-25 15:04:57 -0400

    15/ chore: moved template components to seperate folder

--- nextjsremake/app/page.tsx
+++ nextjsremake/app/page.tsx
@@ -1,10 +1,10 @@
-import { DeployButton } from "@/components/deploy-button";
-import { EnvVarWarning } from "@/components/env-var-warning";
-import { AuthButton } from "@/components/auth-button";
-import { Hero } from "@/components/hero";
-import { ThemeSwitcher } from "@/components/theme-switcher";
-import { ConnectSupabaseSteps } from "@/components/tutorial/connect-supabase-steps";
-import { SignUpUserSteps } from "@/components/tutorial/sign-up-user-steps";
+import { DeployButton } from "@/components/template/deploy-button";
+import { EnvVarWarning } from "@/components/template/env-var-warning";
+import { AuthButton } from "@/components/template/auth-button";
+import { Hero } from "@/components/template/hero";
+import { ThemeSwitcher } from "@/components/template/theme-switcher";
+import { ConnectSupabaseSteps } from "@/components/template/tutorial/connect-supabase-steps";
+import { SignUpUserSteps } from "@/components/template/tutorial/sign-up-user-steps";
 import { hasEnvVars } from "@/lib/utils";
 import Link from "next/link";
 
--- nextjsremake/components/template/update-password-form.tsx
+++ nextjsremake/components/template/update-password-form.tsx
@@ -2,16 +2,16 @@
 
 import { cn } from "@/lib/utils";
 import { createClient } from "@/lib/supabase/client";
-import { Button } from "@/components/ui/button";
+import { Button } from "@/components/template/ui/button";
 import {
   Card,
   CardContent,
   CardDescription,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+} from "@/components/template/ui/card";
+import { Input } from "@/components/template/ui/input";
+import { Label } from "@/components/template/ui/label";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 
